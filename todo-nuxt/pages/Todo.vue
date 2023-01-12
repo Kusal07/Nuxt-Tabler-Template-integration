2abb6af642efb4cc625e86c3aac33472ed47b34d
Kusal Fernando <it20264666@my.sliit.lk>
2023-01-12 19:45:02 +0530

    update

--- todo-nuxt/pages/Todo.vue
+++ todo-nuxt/pages/Todo.vue
@@ -48,55 +48,6 @@
         </table>
       </div>
     </div>
-    <!-- <div class="container">
-      <div class="row">
-          <div class="col-lg-12 text-center">
-              <h1 class="page-title">My Todo List</h1>
-          </div>
-  
-          <div class="col-lg-12 mt-5">
-              <form action="" method="post" enctype="multipart/form-data">
-                  <div class="row">
-                      <div class="col-lg-8">
-                          <div class="form-group">
-                              <input class="form-control" name="title" type="text" placeholder="Enter Task" v-model="title" required>
-                          </div>
-                      </div>
-                  <div class="col-lg-4">
-                      <button type="submit" class="btn btn-success" @click="submit">Submit</button>
-                  </div>
-                  </div>
-              </form>
-          </div>
-          <div class="col-lg-12 mt-5">
-              <div>
-                  <table class="table table-striped">
-                      <thead>
-                        <tr>
-                          <th scope="col">#</th>
-                          <th scope="col">Title </th>
-                          <th scope="col">Status</th>
-                          <th scope="col">Action</th>
-                        </tr>
-                      </thead>
-                      <tbody>
-                        <tr v-for="task in tasks">
-                          <td scope="row">{{ task.id }}</td>
-                          <td>{{ task.title }}</td>
-                          <td>
-                            <span v-if="task.done==0" class="badge bg-warning">Not Completed</span>
-                            <span v-else class="badge bg-success">Completed</span>
-                          </td>
-                          <td>
-                            <button class="btn btn-danger" @click="deletetasks(task.id)">Remove</button>
-                          </td>
-                        </tr>
-                      </tbody>
-                    </table>
-              </div>
-          </div>
-      </div>
-  </div> -->
   </template>
   
   <script>
@@ -134,23 +85,6 @@
           this.getTasks();
         })
       },
-  
-      // deleteTodo:function(id){
-      //   this.$nuxt.$loading.start();
-      //   let api = `${this.$axios.defaults.baseURL}api/tasks/`+id;
-      //   this.$axios.delete(api).then(res=>{
-      //     if(res.data == true){
-      //       this.getTasks();
-      //     }
-      //   }).catch(error=>{
-      //     console.log(error);
-      //   }).finally(()=>{
-      //     // loader stop
-      //     this.$nuxt.$loading.finish();
-      //   });
-      // },
-  
-      
     },
     mounted(){
       this.getTasks();
